c46bdff34a8da7082e28849d6cd19de6268e464d
Imalla Harshitha <imallaharshitha95@gmail.com>
2026-08-21 23:25:50 +0530

    Combine Flutter widget projects

---
+++ PC-51/main.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CommentsPage(),
+    );
+  }
+}
+
+class CommentsPage extends StatefulWidget {
+  const CommentsPage({super.key});
+
+  @override
+  State<CommentsPage> createState() => _CommentsPageState();
+}
+
+class _CommentsPageState extends State<CommentsPage> {
+  List comments = [];
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchComments();
+  }
+
+  Future<void> fetchComments() async {
+    final url = Uri.parse("https://jsonplaceholder.typicode.com/comments");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      setState(() {
+        comments = jsonDecode(response.body);
+        loading = false;
+      });
+    } else {
+      setState(() {
+        loading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return S
